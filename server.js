6e6a116f74aaea94dbf5c9806fd30c0db8f0cb2d
20mthorpe <tho20019@byui.edu>
2024-05-09 14:46:28 -0600

    installing packages

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const baseController = require('./controllers/baseController');
 
