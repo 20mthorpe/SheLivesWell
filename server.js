d251500c13bb2d6e0d79ddd46f2b696858c2e7e3
20mthorpe <tho20019@byui.edu>
2024-05-22 13:00:41 -0600

    not sure?

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const baseController = require('./controllers/baseController');
